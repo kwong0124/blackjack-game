4507244c3fe78cca97cc56df39f6093791c12149
Kathryn Klarich <kathryklarich@gmail.com>
2017-04-24 11:13:55 -0500

    Allow user to play again, refactor code to fix spacing, add doc strings

--- blackjack.py
+++ blackjack.py
@@ -30,34 +30,43 @@ push, and the player does not win or lose money.\n''')
 suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
 numbers = list(range(2, 11))
 royals = ['Ace', 'King', 'Queen', 'Jack']
+
 faces = numbers + royals
 
 deck = []
 player_hand = []
 dealer_hand = []
+player_points = 0
+dealer_points = 0
 
 deck = list(itertools.product(faces, suits))
 random.shuffle(deck)
 
-# Dict to look up points for royal faces
 royals_dict = {'Ace': 1, 'King': 10, 'Queen': 10, 'Jack': 10}
 
 def start_playing():
-    # Ask user if they want to play the game - player responds with yes or no.
-    # If yes, call next function, if no, quit program
-    user_input = input('Are you ready to play blackjack? (Y/n): ')
+    '''Ask user if they want to start playing blackjack'''
+
+    user_input = input('\nAre you ready to play blackjack? (Y/n): ')
+
     if user_input.lower() == 'n':
-        print('Quitting game... hope to see you soon!\n')
+
+        print('\nQuitting game... hope to see you soon!\n')
+
     else:
+
         print('\nDealing cards...')
 
 def draw_card(card_deck):
-    '''draws a card from the shuffled deck'''
+    '''Draws a card from the shuffled deck and returns the card'''
 
     card = card_deck.pop(-1)
     return card
 
 def player():
+    '''Sums the player's points by calling the sum_cards function and
+    determines if the player_decision function should be called (i.e. less than
+    21 points), elif the player has a blackjack, or else the player busts.'''
 
     player_points = sum_cards(player_hand)
 
@@ -65,80 +74,142 @@ def player():
     print('\nThe sum of cards in your hand is {}'.format(player_points))
 
     if player_points < 21:
+
         player_decision()
+
     elif player_points == 21:
-        print('\nYou\'ve got 21 points! \n\n***Blackjack!*** \n')
+
+        print('\nYou\'ve got 21 points! \n\n*** Blackjack! *** \n')
+
     else:
+
         print('\nThat\'s a bust... Better luck next time!')
 
 def dealer():
+    '''Similar to player player function. Sums the dealer points by calling the
+    sum_cards function. Determines if dealer_decision function should be
+    called (i.e. dealer has <21 points) elif the dealer has a blackjack, else
+    the dealer busts'''
 
     dealer_points = sum_cards(dealer_hand)
 
     if dealer_points < 21:
+
         dealer_decision(dealer_points)
+
     elif dealer_points == 21:
-        print('Dealer blackjack!')
+
+        print('\n*** Dealer blackjack! ***')
+
     else:
-        print('Dealer busts!')
+
+        print('\nDealer busts!')
 
 
 def deal_cards():
     '''deals cards at the beginning of the game'''
 
     while len(player_hand) < 2:
+
         player_hand.append(draw_card(deck))
+
     while len(dealer_hand) < 2:
+
         dealer_hand.append(draw_card(deck))
 
     print('\nThe dealer\'s hand is showing {}'.format(dealer_hand[0]))
 
 def player_decision():
-    # allows the player decide whether to hit or stay
+    '''Allows player to decide whether to hit or stay'''
+
     player_decision = input('\nWould you like to hit or stand? (h/S): ')
 
     if player_decision.lower() == 'h':
+
         new_card = draw_card(deck)
         player_hand.append(new_card)
         print('\nYou drew {}'.format(new_card))
         player()
+
     else:
+
         print('\nDealer\'s turn...')
 
+
 def sum_cards(hand):
+    '''Takes a hand of cards and returns the point value of cards in the hand'''
+
     value_list = []
+
     for card in hand:
+
         if type(card[0]) == int:
+
             value_list.append(card[0])
+
         else:
+
             value_list.append(royals_dict[card[0]])
+
     return sum(value_list)
 
+
 def dealer_decision(points):
+    '''Draws cards until the dealer hand point total is greater than 17, has a
+    blackjack or busts (whichever comes first). Determines who wins by comparing
+    dealer_hand and player_hand'''
+
     while points < 17:
+
         new_card = draw_card(deck)
         dealer_hand.append(new_card)
         print('\nThe dealer drew {}.'.format(new_card))
         points = sum_cards(dealer_hand)
+
     if points == 21:
-        print('\n***Dealer blackjack!*** \n\nBetter luck next time...\n')
+
+        print('\n*** Dealer blackjack! *** \n\nBetter luck next time...\n')
 
     else:
 
         print('\n The dealer\'s hand contains:\n\n {} \n\nThe dealer has {} points\
-    ... You have {} points'.format(\
+... You have {} points'.format(\
         dealer_hand, sum_cards(dealer_hand), sum_cards(player_hand)))
 
         if sum_cards(dealer_hand) > 21:
-            print('Dealer busts ... you win!')
+
+            print('\nDealer busts ... you win!')
+
         elif sum_cards(dealer_hand) < 21 and sum_cards(player_hand) > sum_cards(dealer_hand):
-            print('You win!')
+
+            print('\nYou win!')
+
         elif sum_cards(dealer_hand) <21 and sum_cards(dealer_hand) > sum_cards(player_hand):
-            print('Dealer wins! ... Better luck next time!')
+
+            print('\nDealer wins! ... Better luck next time!')
+
         elif sum_cards(dealer_hand) == sum_cards(player_hand):
-            print('It\'s a tie! ... Better luck next time.')
+
+            print('\nIt\'s a tie! ... Better luck next time.')
+
         else:
-            print('Dealer blackjack! ... Better luck next time')
+
+            print('\nDealer blackjack! ... Better luck next time')
+
+
+def play_again():
+    '''ask the user if he/she wants to play again. If yes, call main function,
+    if no, exit the game'''
+
+    response = input('\nWould you like to play again? (Y/n): ')
+
+    if response.lower() == 'n':
+
+        print('\nThanks for playing Blackjack game!\n')
+    else:
+
+        print('\nStarting new game...Good luck!')
+        main()
 
 def main():
 
@@ -149,9 +220,23 @@ def main():
     player()
 
     if sum_cards(player_hand) < 21:
+
         while len(dealer_hand) < 2:
+            
             dealer_hand.append(draw_card(deck))
+
         dealer()
 
+    while len(player_hand) > 0:
+
+        player_hand.pop()
+
+    while len(dealer_hand) > 0:
+
+        dealer_hand.pop()
+
+    play_again()
+
 if __name__ == '__main__':
+
     main()
